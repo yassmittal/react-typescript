6293ae9e4bd12979739efa184ee40ff77492d2f6
satyam <satyam@satyam-ka-Mac-mini.local>
2024-05-28 12:01:48 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useEffect, useState } from "react";
+import axios from "axios";
+import User from "./components/User";
+import { AppProps, Users } from "./App.types";
 
-function App() {
+const App: FC<AppProps> = ({ title }) => {
+  const [users, setUsers] = useState<Users[]>([]);
+  const [isLoading, setisLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setisLoading(true);
+      const { data } = await axios.get("https://randomuser.me/api/?results=10");
+      console.log(data);
+      setUsers(data.results);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setisLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>{title}</h1>
+      <button onClick={handleClick}>Show Users</button>
+      {isLoading && <p>Loading...</p>}\
+      <ul>
+        {users.map(({ login, name, email }) => {
+          return <User name={name} email={email} key={login.uuid} />;
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
